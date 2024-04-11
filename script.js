294075c46a30479461ce8882d2fcb3b06c879f57
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-04-11 13:18:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const counter = document.getElementById("counter");
+const butn = document.getElementById("incrementBtn");
+
+butn.addEventListner("click",function() {
+	let count = parseInt(counter.textContent);
+	
+})
