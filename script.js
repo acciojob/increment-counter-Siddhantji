b66055b07f0bd4428a5bcd7fb786f204b646f5a0
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-04-11 13:21:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ const butn = document.getElementById("incrementBtn");
 
 butn.addEventListner("click",function() {
 	let count = parseInt(counter.textContent);
-	
-})
+	alert(count);
+	count++;
+	counter.textContent=count;
+	})
